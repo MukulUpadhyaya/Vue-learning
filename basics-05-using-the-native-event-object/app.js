fca0203b2040af47e78624d3fbdc4d98eb2e1726
Mukul Upadhyaya <mukulfb49@gmail.com>
2024-09-23 17:19:48 +0530

    Watchers

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -16,6 +16,13 @@ const app = Vue.createApp({
       }
     }
   },
+  watch: {
+    counter(value){
+      if(value>50){
+        this.counter = 0;
+      }
+    }
+  },
   methods: {
     setName(event) {
       this.name = event.target.value ;
