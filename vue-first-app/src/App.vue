102aa5810b8d541609995158fbc2e5e63c52028f
Mukul Upadhyaya <mukulfb49@gmail.com>
2024-09-26 16:27:17 +0530

    Workflow with the Vue CLI

---
+++ vue-first-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <header><h1>My Friends</h1></header>
+    <ul>
+        <friend-contact></friend-contact>
+        <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+        return {
+            friend: {
+                id: 'Mukul',
+                name: 'Mukul Upadhyaya',
+                phone: '09639136578',
+                email: 'mukulfb49@gmail.com',
+            },
+        }
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+</style>
