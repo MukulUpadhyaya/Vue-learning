2a8727320999bfd95a311852ce17a4fc64f8c84b
Mukul Upadhyaya <mukulfb49@gmail.com>
2024-10-03 17:19:57 +0530

    learned about form with vue ✏️

---
+++ forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,51 @@
+<template>
+    <ul>
+        <li :class="{active:modelValue === 'Poor'}"><button type="button" @click="activateButton('Poor')">Poor</button></li>
+        <li :class="{active:modelValue === 'Average'}"><button type="button" @click="activateButton('Average')">Average</button></li>
+        <li :class="{active:modelValue === 'Great'}"><button type="button" @click="activateButton('Great')">Great</button></li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props:['modelValue'],
+        emits:['update:modelValue'],
+        methods:{
+            activateButton(option){
+                this.$emit('update:modelValue', option);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.active {
+	border-color: blueviolet;
+}
+
+.active button {
+	color: blueviolet;
+}
+
+ul {
+	list-style: none;
+	margin: 0.5rem 0;
+	padding: 0;
+	display: flex;
+}
+
+li {
+	margin: 0 1rem;
+	border: 2px solid gray;
+	padding: 1rem;
+	display: flex;
+	align-items: center;
+}
+
+button {
+	font: inherit;
+	border: none;
+	background: transparent;
+	cursor: pointer;
+}
+</style>
