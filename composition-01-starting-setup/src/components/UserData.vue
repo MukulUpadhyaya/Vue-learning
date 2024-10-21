77edb565ed3a2b97fe43b0d2300f15d2999e29d3
Mukul Upadhyaya <mukulfb49@gmail.com>
2024-10-21 11:23:00 +0530

    composition API

---
+++ composition-01-starting-setup/src/components/UserData.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>{{ userName }}</h2>
+        <h2>{{ user.age }}</h2>
+    </div>
+</template>
+
+<script setup>
+
+import { computed, defineProps, inject } from 'vue';
+
+const props = defineProps({
+    firstName: String,
+    lastName: String
+})
+
+const user = inject('user');
+
+const userName = computed(() => {
+    return props.firstName + ' ' + props.lastName;
+})
+// computed: {
+//     userName(){
+//         return this.firstName + ' ' + this.lastName;
+//     }
+// }
+
+</script>
+
+<style scoped></style>
